ccc4da6f0abdc24cb917db6d3d773a0a161f0d00
pavan kenguva <mongamnarendra@gmail.com>
2024-04-01 23:11:48 +0530

    js ocmpleted

--- pass.js
+++ pass.js
@@ -1,30 +1,69 @@
 
+//this function displays the value of the range 
+
+var range=8;
+function getValue()
+{
+    range=document.getElementById("range").value;
+    document.getElementById("length").innerHTML=range;
+}
+
+
+//main logic
 
 var pass=[];
-
-function makePass(){
-    for(let i=0;i<8;i++)
+var nchecked=3;
+function makePass()
+{
+    document.getElementById("password").value="";
+    var find= document.getElementById("all").checked;
+    while(pass.length < range)
     {
-        var num=Math.floor(Math.random()*3)
-        
-        switch(num)
+        var num=Math.floor(Math.random()*nchecked)
+
+         
+        if((document.getElementById("lower").checked == true || find ==true)  && num==0)
         {
-           
-            case 1:var up=upperPass();
-                   pass.push(up);
-                   break;
-            case 2:var lp=lowerPass();
-                   pass.push(lp)
-                   break;
-            case 3:var np=numberPass();
-                   pass.push(np);
-                    break;
-            default:alert(1);
-       }
-       alert(pass)
+            pass.push(upperPass())
+        }
+
+        if((document.getElementById("upper").checked ==  true || find ==true) && num==1)
+        {
+            pass.push(lowerPass())
+        }
+
+        if((document.getElementById("number").checked == true || find ==true) && num == 2)
+        {
+            pass.push(numberPass())
+        }
     }
-    // alert(pass)
+
+    displayPass()
+   
 }
+
+//displays the password to the users
+
+var strongPass="";
+
+function displayPass()
+{
+    
+    for(let i in pass)
+    {
+        strongPass+=pass[i]
+    }
+
+   document.getElementById("password").value=strongPass
+   strongPass="";
+   pass=[]
+   
+}
+
+
+
+
+//functions that generates uppercase , lowercase and number randomly
 
 function upperPass()
 {
@@ -39,4 +78,28 @@ function lowerPass()
 function numberPass()
 {
     return(Math.floor(Math.random()*(9 - 0)+0))
+}
+
+
+
+
+
+// to change the checkbox values if all is true then remaining checkboxes changed to false
+
+function change()
+{
+    if(document.getElementById("all").checked == true)
+    {
+        document.getElementById("all").checked = false
+    }
+    
+}
+
+function allChanges()
+{
+
+        document.getElementById("lower").checked=false;
+        document.getElementById("upper").checked=false;
+        document.getElementById("number").checked=false;
+    
 }
